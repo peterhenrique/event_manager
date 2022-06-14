efd0cd819fbbaa13e8b75ef1c69c7839c6d574a8
peterhenrique <pedrohsdeoliveira27@gmail.com>
2022-06-14 14:38:14 -0300

    README.md

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # event_manager
-In here I'll have my first contact with Files and Serialization. Differently from other projects, this one is made by following a tutorial from The Odin Project.
+In here I'll have my first contact with Files and Serialization. 
+Differently from other projects, this one is made by following a tutorial from The Odin Project.
+
+In here I've had the first contact with an API - the google API of Civic Info - with ERB and reading content from a CSV file. 
+
+I've struggled a lot on managing how to get date, time and weekdays from it. 
+
---
+++ lib/event_manager.rb
@@ -0,0 +1,102 @@
+require 'csv'
+require 'google/apis/civicinfo_v2'
+require 'erb'
+require 'pry-byebug'
+require 'time'
+require 'date'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+def count_frequency(array)
+  array.max_by {|a| array.count(a)}
+end
+
+
+
+
+def phoner (phone_number)
+  phone_number.gsub!(/[^\d]/, '')
+  if phone_number.length == 10
+    phone_number
+  elsif phone_number.length == 11 && phone_number[0] == 1
+    phone_number[1..11]
+  else
+    puts 'bad number'
+  end
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id,form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+puts 'EventManager initialized.'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
+@hour = []
+
+@dia = []
+
+j = 0
+
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
+  phone_number = phoner(row[:homephone])
+  reg_date = row[:regdate]
+
+
+
+
+
+  printer = DateTime.strptime(reg_date, "%m/%d/%y %R")
+
+  @hour[j] = printer.hour
+  @dia[j] = printer.wday
+
+  j +=1
+
+
+
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id,form_letter)
+end
+
+puts "#{count_frequency(@hour)}"
+
+puts "#{count_frequency(@dia)}"
+
